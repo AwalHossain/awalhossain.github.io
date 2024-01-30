8d5816c15662a8fffd211601bff41e569bd015a4
Awalhossain <waqin21@gmail.com>
2024-01-30 09:10:42 +0600

    Add Image and FluidImage types

---
+++ src/types/Image.ts
@@ -0,0 +1,15 @@
+
+
+export type Image = {
+    srcPath?: string,
+    caption?: string,
+};
+
+// The type that is being returned after using the {...GatsbyImageSharpFluid} GraphQL operator.
+export type FluidImage = {
+    base64: string | null;
+    aspectRatio: number;
+    src: string;
+    srcSet: string;
+    sizes: string;
+};
