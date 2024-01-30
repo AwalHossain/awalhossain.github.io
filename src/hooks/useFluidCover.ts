e2f853a72218bf6127dac0e12c83ec7893073039
Awalhossain <waqin21@gmail.com>
2024-01-30 15:35:52 +0600

    Add useFluidCover hook for handling fluid covers ☑️

---
+++ src/hooks/useFluidCover.ts
@@ -0,0 +1,30 @@
+import { IGatsbyImageData } from 'gatsby-plugin-image';
+// import { UseImagesQuery } from './generated/UseImagesQuery';
+import { useFluidCovers } from './useFluidCovers';
+
+type UseFluidCoverProps = {
+    imagePath?: string | null | undefined,
+};
+
+export const useFluidCover = (
+    props: UseFluidCoverProps,
+): IGatsbyImageData | undefined | null => {
+    const { imagePath } = props;
+    const allImages = useFluidCovers();
+
+    if (!imagePath) {
+        return undefined;
+    }
+
+    const foundNode = allImages.find(
+        (node: any) => {
+            return node.relativePath === imagePath;
+        },
+    );
+
+    if (!foundNode) {
+        return undefined;
+    }
+
+    return foundNode?.childImageSharp?.gatsbyImageData;
+};
