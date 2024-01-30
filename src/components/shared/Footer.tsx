a3f370bfcd985feb7abbbe1f93eca89f712eca4f
Awalhossain <waqin21@gmail.com>
2024-01-30 16:59:19 +0600

    Add Footer component ✅

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { profile } from '../../data/profile';
+import SocialLinks from './socialLinks';
+type FooterProps = {
+    className?: string,
+    withFeedback?: boolean,
+    withSupport?: boolean,
+};
+
+const Footer = (props: FooterProps): React.ReactElement => {
+    const { className = '', withFeedback = false, withSupport = true } = props;
+
+    return (
+        <footer className={`${className}`}>
+
+            <div
+                className="flex flex-row items-center justify-center"
+            >
+                <SocialLinks
+                    links={profile?.socialLinks}
+                    expandable={false}
+                    iconClassName="w-5 h-5"
+                    itemClassName="mr-2 ml-2"
+                />
+            </div>
+
+            <div style={{ flex: 1 }} className="hidden sm:flex">
+                &nbsp;
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
