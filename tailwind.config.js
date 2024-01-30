bf09c362bf08e761246fe96cffee02f7f4a77aa2
Awalhossain <waqin21@gmail.com>
2024-01-30 15:29:33 +0600

    Update Tailwind configuration 🌐

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
-/** @type {import('tailwindcss').Config} */
+// @see: https://tailwindcss.com/docs/configuration/
+// @see: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
-  ],
+  // Reminder! Tailwind can't recognize conditional classes when using purge.
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Roboto',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      boxShadow: {
+        // The shadow-card class that is used for Cards.
+        // @see: https://tailwindcss.com/docs/box-shadow
+        card: '0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)',
+      },
+      // Tailwinds typography customization
+      // @see: https://github.com/tailwindlabs/tailwindcss-typography#customization
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            // color: '#0FF',
+            a: {
+              color: theme('colors.red.600'),
+              '&:hover': {
+                color: theme('colors.red.500'),
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    // eslint-disable-next-line global-require
+    require('@tailwindcss/typography'),
+  ],
+};
