b1d897de7e06e0ba00115a83c76ed3d363025561
Awalhossain <waqin21@gmail.com>
2024-01-30 15:33:27 +0600

    Add Location component✅

---
+++ src/components/shared/Location.tsx
@@ -0,0 +1,21 @@
+import { FiMapPin } from '@react-icons/all-files/fi/FiMapPin';
+import React from 'react';
+
+import type { Location as LocationType } from '../../types/Location';
+
+type LocationProps = {
+    location: LocationType,
+};
+
+const Location = (props: LocationProps): React.ReactElement => {
+    const { location } = props;
+
+    return (
+        <div className="flex flex-row items-center font-light">
+            <FiMapPin className="mr-1 w-4 h-4" />
+            <div>{location.name}</div>
+        </div>
+    );
+};
+
+export default Location;
