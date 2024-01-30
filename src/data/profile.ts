f1a7ae1dac773215c70423dbc4b4f7e825ee496d
Awalhossain <waqin21@gmail.com>
2024-01-30 15:35:11 +0600

    Add profile data to src/data/profile.ts ✅

---
+++ src/data/profile.ts
@@ -0,0 +1,26 @@
+//@ts-ignore
+import { socialLinks } from './socialLinks';
+
+export const profile = {
+    firstName: 'Awal',
+    lastName: 'Hossain',
+    position: 'Currently Seeking a Job',
+    summary: [
+        'Author of 170k ★️ javascript-algorithms repo on GitHub',
+    ],
+    avatar: {
+        srcPath: 'avatar.jpg',
+        caption: 'Awal Hossain',
+    },
+    location: {
+        name: 'Dhaka, The Bangladesh',
+    },
+    tags: [
+        { name: 'Full Stack' },
+        { name: 'JavaScript' },
+        { name: 'Typescript' },
+        { name: 'React' },
+        { name: 'Node' },
+    ],
+    socialLinks,
+};
