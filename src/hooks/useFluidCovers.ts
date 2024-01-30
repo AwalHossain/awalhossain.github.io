18bd604322e76631da4431e3fa893d3a4727a818
Awalhossain <waqin21@gmail.com>
2024-01-30 15:36:04 +0600

    Add useFluidCovers hook for handling fluid covers ✅

---
+++ src/hooks/useFluidCovers.ts
@@ -0,0 +1,30 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useFluidCovers = () => {
+    // @see: https://www.gatsbyjs.com/plugins/gatsby-plugin-image/
+    const images = useStaticQuery(graphql`
+    query UseImagesQuery {
+      allFile(
+        filter: {sourceInstanceName: {eq: "images"}}
+      ) {
+        nodes {
+          base
+          name
+          relativePath
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH,
+              quality: 95,
+              transformOptions: {
+                fit: COVER,
+                cropFocus: CENTER,
+                grayscale: false,
+              },
+            )
+          }
+        }
+      }
+    }
+  `);
+    return images.allFile.nodes;
+};
