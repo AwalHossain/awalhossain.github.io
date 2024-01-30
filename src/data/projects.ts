3d2e16dcb6f50288a04cf5a30e201b7104d1fa55
Awalhossain <waqin21@gmail.com>
2024-01-30 20:56:15 +0600

    Add Reely- A MERN Video Streaming App project ✅

---
+++ src/data/projects.ts
@@ -0,0 +1,50 @@
+
+
+
+export const project = {
+    'Reely- A MERN Video Streaming App': {
+        id: 'Mern Video Streaming App',
+        name: '🎥 Reely- A MERN Video Streaming App',
+        Client: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
+        server: {
+            urr: "https://github.com/AwalHossain/video_streaming_client"
+        },
+        demoURL: { url: 'https://reely.tech' },
+        cover: { srcPath: 'projects/video-streaming/cover.png' },
+        startDate: '2023-06-25',
+        endDate: '2024-01-25',
+        summary: [
+            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming, offering a comprehensive, full-stack solution utilizing the latest MERN stack technologies. With MERN Video Streaming, users can easily upload, manage, and stream videos on demand, providing a seamless experience. This project features a video processing capability that provides real-time notifications upon completion',
+        ],
+        tags: [
+            { name: 'JavaScript' },
+            { name: 'TypeScript' },
+            { name: 'Redux Toolkit' },
+            { name: 'NodeJs' },
+            { name: 'ExpressJs' },
+            { name: 'ExpressJs' },
+            { name: 'Socket.io' },
+            { name: 'BullMQ' },
+            { name: 'MongoDB' },
+            { name: 'Redis' },
+        ],
+        gitHubRepo: {
+            owner: 'AwalHossain',
+            repo: 'video_streaming_service_server',
+        },
+        achievements: [
+            {
+                title: 'Video upload and processing',
+                image: { srcPath: 'projects/video-streaming/upload.png' },
+                date: '2024-01-30',
+                link: { url: 'https://news.ycombinator.com/front?day=2021-04-16', caption: 'Hacker News archive' },
+            },
+            {
+                title: 'Video streaming',
+                image: { srcPath: 'projects/video-streaming/player.png' },
+                date: '2021-01-30',
+                link: { url: 'https://www.producthunt.com/posts/js-image-carver', caption: 'Product page' },
+            },
+        ],
+    },
+}
