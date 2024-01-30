c9a596d3c15c9e19d87af48a0f6baa24acc78eb7
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-01-30 02:14:14 +0000

    Add new components and update dependencies

---
+++ src/components/Layout/PageLayout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../shared/Header'
+
+const PageLayout = () => {
+    return (
+        <main>
+            <div>
+                <Header />
+                <article>
+
+                </article>
+            </div>
+        </main>
+    )
+}
+
+export default PageLayout
---
+++ src/data/profile.js
@@ -0,0 +1,26 @@
+
+
+export const profile = {
+    firstName: 'Awal',
+    lastName: 'Hossain',
+    position: 'Currently Seeking a Job',
+    summary: [
+        'Author of 170k ★️ javascript-algorithms repo on GitHub',
+    ],
+    avatar: {
+        // srcPath: 'profile/avatar_500x500.jpg',
+        srcPath: 'profile/avatar_500x500_v2.jpg',
+        caption: 'Awal Hossain',
+    },
+    location: {
+        name: 'Dhaka, The Bangladesh',
+    },
+    tags: [
+        { name: 'Full Stack' },
+        { name: 'JavaScript' },
+        { name: 'Typescript' },
+        { name: 'React' },
+        { name: 'Node' },
+    ],
+    socialLinks,
+};
