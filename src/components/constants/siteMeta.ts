2c10002f1f05f8842280c576b10b71c49fcc32c9
Awalhossain <waqin21@gmail.com>
2024-01-30 15:30:36 +0600

    Add site metadata constants

---
+++ src/components/constants/siteMeta.ts
@@ -0,0 +1,9 @@
+export const siteURL = 'https://trekhleb.dev';
+export const defaultSiteTitle = 'Trekhleb.dev';
+export const defaultSiteDescription = 'Personal blog and projects by Oleksii Trekhleb';
+export const brandName = 'Awal';
+export const windowNamePrefix = brandName;
+export const windowNameSeparator = '|';
+export const twitterUser = '@Trekhleb';
+// Path to the image that is placed in the 'static' folder.
+export const siteImage = '/static-assets/images/site-meta-image-01.png';
