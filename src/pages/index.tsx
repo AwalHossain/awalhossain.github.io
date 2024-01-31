57054065c7025d86a0567189cbb213a2f9ea05e0
Awalhossain <waqin21@gmail.com>
2024-02-01 01:19:38 +0600

    Fix import typo in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AboutScreen from '../components/screens/AboutScreens';
+import AboutScreen from '../components/screens/AboutScreen';
 import { profile } from '../data/profile';
 
 const Index = (): React.ReactElement => {
