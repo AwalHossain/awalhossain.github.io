824ff75618bf3af929337e2253685b9797715350
Awalhossain <waqin21@gmail.com>
2024-02-01 01:19:52 +0600

    Add Project component

---
+++ src/templates/Project.tsx
@@ -0,0 +1,30 @@
+import { PageProps } from 'gatsby';
+import * as React from 'react';
+import ProjectScreen from '../components/screens/ProjectScreen';
+import { projects } from '../data/projects';
+import { ProjectID, Project as ProjectType } from '../types/Project';
+
+interface ProjectProps extends PageProps {
+    pageContext: {
+        projectID: ProjectID,
+    },
+}
+
+const Project = (props: ProjectProps): React.ReactElement => {
+    const { pageContext } = props;
+    const { projectID = null } = pageContext;
+
+    const project: ProjectType | null = projectID && (projectID in projects)
+        // @ts-ignore
+        ? projects[projectID]
+        : null;
+
+    console.log(project, projectID, 'project and projectID');
+
+
+    return (
+        <ProjectScreen project={project} />
+    );
+};
+
+export default Project;
