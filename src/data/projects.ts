83ab6382218783f182056818fd93964d0f8bff60
Awalhossain <waqin21@gmail.com>
2024-02-01 01:19:33 +0600

    Refactor project names and URLs in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,20 +1,20 @@
 
 
 
-export const project = {
-    'Reely- A MERN Video Streaming App': {
-        id: 'Mern Video Streaming App',
+export const projects = {
+    'Reely-A-MERN-Video-Streaming-App': {
+        id: 'Reely-A-MERN-Video-Streaming-App',
         name: '🎥 Reely- A MERN Video Streaming App',
-        Client: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
-        server: {
-            urr: "https://github.com/AwalHossain/video_streaming_client"
+        server: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
+        client: {
+            url: "https://github.com/AwalHossain/video_streaming_client"
         },
         demoURL: { url: 'https://reely.tech' },
         cover: { srcPath: 'projects/video-streaming/cover.png' },
         startDate: '2023-06-25',
         endDate: '2024-01-25',
         summary: [
-            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming, offering a comprehensive, full-stack solution utilizing the latest MERN stack technologies. With MERN Video Streaming, users can easily upload, manage, and stream videos on demand, providing a seamless experience. This project features a video processing capability that provides real-time notifications upon completion',
+            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
         ],
         tags: [
             { name: 'JavaScript' },
@@ -32,7 +32,7 @@ export const project = {
             owner: 'AwalHossain',
             repo: 'video_streaming_service_server',
         },
-        achievements: [
+        details: [
             {
                 title: 'Video upload and processing',
                 image: { srcPath: 'projects/video-streaming/upload.png' },
@@ -47,19 +47,19 @@ export const project = {
             },
         ],
     },
-    'Calling Apps': {
-        id: 'Mern Video Streaming App',
-        name: '🎥 Reely- A MERN Video Streaming App',
-        Client: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
-        server: {
-            urr: "https://github.com/AwalHossain/video_streaming_client"
+    'BookKeeper-a-book-management-site': {
+        id: 'BookKeeper-a-book-management-site',
+        name: '📚 BookKeeper- a book management site',
+        server: { url: "https://github.com/AwalHossain/Bookish" },
+        client: {
+            url: "https://github.com/AwalHossain/Bookish"
         },
-        demoURL: { url: 'https://reely.tech' },
+        demoURL: { url: "https://bookishly.netlify.app/" },
         cover: { srcPath: 'projects/video-streaming/cover.png' },
         startDate: '2023-06-25',
         endDate: '2024-01-25',
         summary: [
-            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming, offering a comprehensive, full-stack solution utilizing the latest MERN stack technologies. With MERN Video Streaming, users can easily upload, manage, and stream videos on demand, providing a seamless experience. This project features a video processing capability that provides real-time notifications upon completion',
+            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
         ],
         tags: [
             { name: 'JavaScript' },
@@ -77,7 +77,97 @@ export const project = {
             owner: 'AwalHossain',
             repo: 'video_streaming_service_server',
         },
-        achievements: [
+        details: [
+            {
+                title: 'Video upload and processing',
+                image: { srcPath: 'projects/video-streaming/upload.png' },
+                date: '2024-01-30',
+                link: { url: 'https://news.ycombinator.com/front?day=2021-04-16', caption: 'Hacker News archive' },
+            },
+            {
+                title: 'Video streaming',
+                image: { srcPath: 'projects/video-streaming/player.png' },
+                date: '2021-01-30',
+                link: { url: 'https://www.producthunt.com/posts/js-image-carver', caption: 'Product page' },
+            },
+        ],
+    },
+    'BookKeeper-a-book-management-site-2': {
+        id: 'BookKeeper-a-book-management-site-2',
+        name: '📚 BookKeeper- a book management site',
+        server: { url: "https://github.com/AwalHossain/Bookish" },
+        client: {
+            url: "https://github.com/AwalHossain/Bookish"
+        },
+        demoURL: { url: "https://bookishly.netlify.app/" },
+        cover: { srcPath: 'projects/video-streaming/cover.png' },
+        startDate: '2023-06-25',
+        endDate: '2024-01-25',
+        summary: [
+            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
+        ],
+        tags: [
+            { name: 'JavaScript' },
+            { name: 'TypeScript' },
+            { name: 'Redux Toolkit' },
+            { name: 'NodeJs' },
+            { name: 'ExpressJs' },
+            { name: 'ExpressJs' },
+            { name: 'Socket.io' },
+            { name: 'BullMQ' },
+            { name: 'MongoDB' },
+            { name: 'Redis' },
+        ],
+        gitHubRepo: {
+            owner: 'AwalHossain',
+            repo: 'video_streaming_service_server',
+        },
+        details: [
+            {
+                title: 'Video upload and processing',
+                image: { srcPath: 'projects/video-streaming/upload.png' },
+                date: '2024-01-30',
+                link: { url: 'https://news.ycombinator.com/front?day=2021-04-16', caption: 'Hacker News archive' },
+            },
+            {
+                title: 'Video streaming',
+                image: { srcPath: 'projects/video-streaming/player.png' },
+                date: '2021-01-30',
+                link: { url: 'https://www.producthunt.com/posts/js-image-carver', caption: 'Product page' },
+            },
+        ],
+    },
+    'BookKeeper-a-book-management-site-3': {
+        id: 'BookKeeper-a-book-management-site-3',
+        name: '📚 BookKeeper- a book management site',
+        server: { url: "https://github.com/AwalHossain/Bookish" },
+        client: {
+            url: "https://github.com/AwalHossain/Bookish"
+        },
+        demoURL: { url: "https://bookishly.netlify.app/" },
+        cover: { srcPath: 'projects/video-streaming/cover.png' },
+        startDate: '2023-06-25',
+        endDate: '2024-01-25',
+        summary: [
+            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
+        ],
+        tags: [
+            { name: 'JavaScript' },
+            { name: 'TypeScript' },
+            { name: 'Redux Toolkit' },
+            { name: 'NodeJs' },
+            { name: 'ExpressJs' },
+            { name: 'ExpressJs' },
+            { name: 'Socket.io' },
+            { name: 'BullMQ' },
+            { name: 'MongoDB' },
+            { name: 'Redis' },
+        ],
+        gitHubRepo: {
+            owner: 'AwalHossain',
+            repo: 'video_streaming_service_server',
+        },
+        details: [
             {
                 title: 'Video upload and processing',
                 image: { srcPath: 'projects/video-streaming/upload.png' },
