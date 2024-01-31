9f034328d31b6db23e952d168ce57729d43babc6
Awalhossain <waqin21@gmail.com>
2024-02-01 01:11:49 +0600

    Add createPages function to create project pages 📝✅

---
+++ gatsby-node.ts
@@ -0,0 +1,26 @@
+import { CreatePagesArgs } from "gatsby";
+import * as path from 'path';
+import { routes } from "./src/components/constants/routes";
+import { projects } from "./src/data/projects";
+import { Project } from "./src/types/Project";
+import { projectMapToArray } from "./src/utils/projects";
+
+async function createProjectPages(args: CreatePagesArgs): Promise<void> {
+    const { actions } = args;
+    const { createPage } = actions;
+    projectMapToArray(projects).forEach((project: Project) => {
+        console.log(project.id, "checking project");
+
+        createPage({
+            path: `${routes.projects.path}/${project.id}`,
+            component: path.resolve('./src/templates/Project.tsx'),
+            context: {
+                projectID: project.id,
+            },
+        });
+    });
+}
+
+export async function createPages(args: CreatePagesArgs): Promise<void> {
+    await createProjectPages(args);
+}
