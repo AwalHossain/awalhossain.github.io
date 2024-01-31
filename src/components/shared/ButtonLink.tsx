4769a780e0b3e580c65b7e35298f15cae45bea49
Awalhossain <waqin21@gmail.com>
2024-02-01 01:17:52 +0600

    Add ButtonLink component

---
+++ src/components/shared/ButtonLink.tsx
@@ -0,0 +1,36 @@
+
+
+
+import React from 'react';
+import HyperLink, { HyperLinkProps } from './HyperLink';
+
+
+type ButtonLinkProps = HyperLinkProps;
+
+
+const ButtonLink = (props: ButtonLinkProps): React.ReactElement => {
+
+    const {
+        className,
+        hoverClassName = '',
+        children,
+        ...rest
+    } = props;
+
+    const hoverButtonClasses = `hover:bg-black hover:text-white ${hoverClassName}`;
+    const buttonClasses = 'bg-white py-1 px-2 rounded shadow-sm border border-solid border-gray-300';
+    const classes = `${className} ${buttonClasses}`;
+
+
+    return (
+        <HyperLink
+            {...rest}
+            className={classes}
+            hoverClassName={hoverButtonClasses}
+        >
+            {children}
+        </HyperLink>
+    );
+};
+
+export default ButtonLink;
