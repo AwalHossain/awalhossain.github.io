2646d735850d7f4f4a517b624da4595cc4958b78
Awalhossain <waqin21@gmail.com>
2024-02-02 01:39:42 +0600

    Add Blog page component

---
+++ src/pages/blog.tsx
@@ -0,0 +1,53 @@
+import { PageProps, graphql } from 'gatsby'
+import React from 'react'
+import BlogScreen from '../components/screens/BlogScreen'
+import { BlogPageQuery } from './__generated__/BlogPageQuery'
+
+interface BlogProps extends PageProps {
+    data: BlogPageQuery
+}
+
+
+export const query = graphql`
+  query BlogPageQuery {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
+      totalCount
+      nodes {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          summary
+          date(formatString: "MMM D, YYYY")
+          cover {
+            childImageSharp {
+              gatsbyImageData(
+                layout: FULL_WIDTH,
+                quality: 95,
+                transformOptions: {
+                  fit: COVER,
+                  cropFocus: CENTER,
+                  grayscale: false,
+                },
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+
+const Blog = (props: BlogProps) => {
+
+    const { data } = props
+
+    return (
+        <BlogScreen posts={data} />
+    )
+}
+
+export default Blog
