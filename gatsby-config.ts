bca265f4eaccd55af8ffaa9bbc60ec99ffc19f53
Awalhossain <waqin21@gmail.com>
2024-02-01 14:54:55 +0600

    Add gatsby-remark-copy-linked-files plugin and configure options

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,7 @@
 import { GatsbyConfig } from 'gatsby';
-import { siteMetadata } from './src/data/siteMetaData';
+import {
+  siteMetadata
+} from './src/data/siteMetadata';
 
 const gatsbyConfig: GatsbyConfig = {
   siteMetadata,
@@ -107,10 +109,93 @@ const gatsbyConfig: GatsbyConfig = {
               },
             },
           },
+
+          // @see: https://www.gatsbyjs.com/plugins/gatsby-remark-copy-linked-files/?=gatsby-remark-copy-linked-files
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              // Ignoring these file extensions since they should be recognized and handled
+              // by the gatsby-transformer-sharp plugin.
+              // @see: https://www.gatsbyjs.com/plugins/gatsby-transformer-sharp/#parsing-algorithm
+              ignoreFileExtensions: ['png', 'jpg', 'jpeg', 'tiff', 'tif', 'webp'],
+              // Save files like `02-demo.gif`
+              // to `public/posts-assets/2a0039f3a61f4510f41678438e4c863a/02-demo.gif`
+              destinationDir: (f: { name: string, hash: string }): string => `posts-assets/${f.hash}/${f.name}`,
+            },
+          },
         ],
       },
     },
 
+    // @see: https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
+    // {
+    //   resolve: 'gatsby-plugin-google-gtag',
+    //   options: {
+    //     // You can add multiple tracking ids and a page-view event will be fired for all of them.
+    //     trackingIds: [
+    //       GOOGLE_ANALYTICS_ID,
+    //     ],
+    //   },
+    // },
+
+    // @see: https://www.gatsbyjs.com/docs/how-to/adding-common-features/adding-an-rss-feed/
+    // {
+    //   resolve: 'gatsby-plugin-feed',
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             ${metaFieldTitle}
+    //             ${metaFieldDescription}
+    //             ${metaFieldSiteUrl}
+    //             site_url: ${metaFieldSiteUrl}
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMdx } }: any): any => {
+    //           return allMdx.edges.map((edge: any): any => {
+    //             const url = site.siteMetadata[metaFieldSiteUrl] + edge.node.fields.slug;
+    //             return {
+    //               ...edge.node.frontmatter,
+    //               description: edge.node.frontmatter.summary,
+    //               date: edge.node.frontmatter.date,
+    //               url,
+    //               guid: url,
+    //               custom_elements: [{ 'content:encoded': edge.node.frontmatter.summary }],
+    //             };
+    //           });
+    //         },
+    //         query: `
+    //           {
+    //             allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+    //               edges {
+    //                 node {
+    //                   fields {
+    //                     slug
+    //                   }
+    //                   frontmatter {
+    //                     title
+    //                     date
+    //                     summary
+    //                   }
+    //                   internal {
+    //                     contentFilePath
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         `,
+    //         output: rssPath,
+    //         title: 'Trekhleb.dev RSS Feed',
+    //       },
+    //     ],
+    //   },
+    // },
   ],
 };
 
