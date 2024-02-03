4584fa08ef23d7d2c09f5b95afb5dbd20d27f50a
Awalhossain <waqin21@gmail.com>
2024-02-03 08:07:57 +0600

    Update siteImage path in siteMeta.ts

--- src/components/constants/siteMeta.ts
+++ src/components/constants/siteMeta.ts
@@ -6,4 +6,4 @@ export const windowNamePrefix = brandName;
 export const windowNameSeparator = '|';
 export const twitterUser = '@Trekhleb';
 // Path to the image that is placed in the 'static' folder.
-export const siteImage = '/static-assets/images/site-meta-image-01.png';
+export const siteImage = '/favicon.ico';
