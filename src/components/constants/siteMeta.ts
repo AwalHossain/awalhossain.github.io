6ee8a2355ee315f5cf93f31ed0060fc0ccf11128
Awalhossain <waqin21@gmail.com>
2024-02-04 15:53:37 +0600

    Update site metadata with new URL and author name

--- src/components/constants/siteMeta.ts
+++ src/components/constants/siteMeta.ts
@@ -1,9 +1,9 @@
-export const siteURL = 'https://trekhleb.dev';
-export const defaultSiteTitle = 'Trekhleb.dev';
-export const defaultSiteDescription = 'Personal blog and projects by Oleksii Trekhleb';
+export const siteURL = 'https://awalhossain.tech';
+export const defaultSiteTitle = 'awalhossain.tech';
+export const defaultSiteDescription = 'Personal blog and projects by Awal Hossain';
 export const brandName = 'Awal';
 export const windowNamePrefix = brandName;
 export const windowNameSeparator = '|';
-export const twitterUser = '@Trekhleb';
+export const twitterUser = '@awalHosssaimn';
 // Path to the image that is placed in the 'static' folder.
 export const siteImage = '/favicon.ico';
