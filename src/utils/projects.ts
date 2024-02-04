abc744f6d7f454b9d7d4a3f6dd48d544d19d9790
Awalhossain <waqin21@gmail.com>
2024-02-04 11:28:33 +0600

    Refactor project-related utility functions

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,23 +1,19 @@
-import { routes } from "../components/constants/routes";
-import { Link } from "../types/Link";
-import { Project, ProjectID, Projects } from "../types/Project";
-
-
+import { routes } from '../components/constants/routes';
+import { Link } from '../types/Link';
+import { Project, ProjectID, Projects } from '../types/Project';
 
 export function projectMapToArray(projects: Projects): Project[] {
-    return Object.keys(projects)
-        .map<Project>((projectID: ProjectID) => {
-            const project: Project = { ...projects[projectID] };
-            // Make sure that the project ID is the same as the project key in the projects map.
-            project.id = projectID;
-            return project;
-        });
+  return Object.keys(projects)
+    .map<Project>((projectID: ProjectID) => {
+      const project: Project = { ...projects[projectID] };
+      // Make sure that the project ID is the same as the project key in the projects map.
+      project.id = projectID;
+      return project;
+    });
 }
 
-
 export function getProjectDetailsLink(projectID: ProjectID): Link {
-
-    return {
-        url: `${routes.projects.path}/${projectID}#details`,
-    };
+  return {
+    url: `${routes.projects.path}/${projectID}#details`,
+  };
 }
