278e57a1d5088c6041364317bdbb9de27e6f51af
Awalhossain <waqin21@gmail.com>
2024-02-04 11:27:30 +0600

    Refactor code to improve performance and readability

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,240 +1,237 @@
-
-
-
 export const projects = {
-    'Reely-A-MERN-Video-Streaming-App': {
-        id: 'Reely-A-MERN-Video-Streaming-App',
-        name: '🎥 Reely- A MERN Video Streaming App',
-        server: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
-        client: {
-            url: "https://github.com/AwalHossain/video_streaming_client"
-        },
-        demoURL: { url: 'https://reely.tech' },
-        cover: { srcPath: 'projects/video-streaming/reely.png' },
-        startDate: '2023-04-25',
-        endDate: '2024-01-25',
-        summary: [
-            'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
-        ],
-        tags: [
-            { name: 'JavaScript' },
-            { name: 'TypeScript' },
-            { name: 'Redux Toolkit' },
-            { name: 'NodeJs' },
-            { name: 'ExpressJs' },
-            { name: 'ExpressJs' },
-            { name: 'Socket.io' },
-            { name: 'BullMQ' },
-            { name: 'MongoDB' },
-            { name: 'Redis' },
-        ],
-        gitHubRepo: {
-            owner: 'AwalHossain',
-            repo: 'video_streaming_service_server',
-        },
-        details: [
-            {
-                title: 'Home Page',
-                image: { srcPath: 'projects/video-streaming/cover.png' },
-                date: '2024-01-30',
-                link: { url: '#', caption: 'video list' },
-            },
-            {
-                title: 'Video upload and processing',
-                image: { srcPath: 'projects/video-streaming/up.png' },
-                date: '2024-01-30',
-                link: { url: 'https://news.ycombinator.com/front?day=2021-04-16', caption: 'Hacker News archive' },
-            },
-            {
-                title: 'Video streaming',
-                image: { srcPath: 'projects/video-streaming/pl.png' },
-                date: '2021-01-30',
-                link: { url: 'https://www.producthunt.com/posts/js-image-carver', caption: 'Product page' },
-            },
-        ],
+  'Reely-A-MERN-Video-Streaming-App': {
+    id: 'Reely-A-MERN-Video-Streaming-App',
+    name: '🎥 Reely- A MERN Video Streaming App',
+    server: { url: 'https://github.com/awalHossain/video_streaming_service_server/' },
+    client: {
+      url: 'https://github.com/AwalHossain/video_streaming_client'
     },
-    'Bookish-A-Book-Management-Site': {
-        id: 'Bookish-A-Book-Management-Site',
-        name: '📚 Bookish- A Book Management Site',
-        server: { url: "https://github.com/AwalHossain/Bookish" },
-        client: {
-            url: "https://github.com/AwalHossain/Bookish"
-        },
-        demoURL: { url: "https://bookishly.netlify.app/" },
-        cover: { srcPath: 'projects/bookish/cover.png' },
-        startDate: '2023-09-18',
-        endDate: '2023-09-27',
-        summary: [
-            'Bookish is a web application that allows users to discover, review, and manage books',
-        ],
-        tags: [
-            { name: 'ReactJS' },
-            { name: 'NodeJs' },
-            { name: 'ExpressJs' },
-            { name: 'MongoDB' },
-            { name: 'RTK' },
-            { name: 'Tailwind CSS' },
-        ],
-        gitHubRepo: {
-            owner: 'AwalHossain',
-            repo: 'video_streaming_service_server',
-        },
-        details: [
-            {
-                title: 'Add new Book to the list',
-                image: { srcPath: 'projects/bookish/addBook.png' },
-                date: '2023-08-30',
-                link: { url: '', caption: '' },
-            },
-            {
-                title: 'Home page',
-                image: { srcPath: 'projects/bookish/home.png' },
-                date: '2023-07-30',
-                link: { url: '', caption: '' },
-            },
-            {
-                title: 'Reading List',
-                image: { srcPath: 'projects/bookish/read.png' },
-                date: '2023-07-30',
-                link: { url: '', caption: '' },
-            },
-        ],
+    demoURL: { url: 'https://reely.tech' },
+    cover: { srcPath: 'projects/video-streaming/reely.png' },
+    startDate: '2023-04-25',
+    endDate: '2024-01-25',
+    summary: [
+      'MERN Video Streaming is a cutting-edge, open-source platform for video streaming',
+    ],
+    tags: [
+      { name: 'JavaScript' },
+      { name: 'TypeScript' },
+      { name: 'Redux Toolkit' },
+      { name: 'NodeJs' },
+      { name: 'ExpressJs' },
+      { name: 'ExpressJs' },
+      { name: 'Socket.io' },
+      { name: 'BullMQ' },
+      { name: 'MongoDB' },
+      { name: 'Redis' },
+    ],
+    gitHubRepo: {
+      owner: 'AwalHossain',
+      repo: 'video_streaming_service_server',
     },
-    'DevBlog-a-developer-blog-site': {
-        id: 'DevBlog-a-developer-blog-site',
-        name: '📝 DevBlog: Developer Community Blog Site',
-        // server: { url: "https://github.com/AwalHossain/Dev-Blog" },
-        client: {
-            url: "https://github.com/AwalHossain/Dev-Blog"
-        },
-        demoURL: { url: "https://bookishly.netlify.app/" },
-        cover: { srcPath: 'projects/devblog/cover.png' },
-        startDate: '2023-06-25',
-        endDate: '2023-07-12',
-        summary: [
-            'Dev-Blog is a user-friendly blog site featuring several blogs on various topics related to development',
-        ],
-        tags: [
-            { name: 'JavaScript' },
-            { name: 'TypeScript' },
-            { name: 'Redux Toolkit' },
-            { name: 'NodeJs' },
-            { name: 'ExpressJs' },
-            { name: 'Tailwind CSS' },
-        ],
-        gitHubRepo: {
-            owner: 'AwalHossain',
-            repo: 'Dev-Blog',
-        },
-        details: [
-            {
-                title: 'Blog post list',
-                image: { srcPath: 'projects/devblog/cover.png' },
-                date: '2023-04-30',
-                link: { url: '', caption: '' },
-            },
-            {
-                title: 'Blog Details',
-                image: { srcPath: 'projects/devblog/detail.png' },
-                date: '2023-04-30',
-                link: { url: '', caption: '' },
-            },
-            {
-                title: 'Save Blog to Reading List',
-                image: { srcPath: 'projects/devblog/save.png' },
-                date: '2023-04-30',
-                link: { url: '', caption: '' },
-            },
-        ],
+    details: [
+      {
+        title: 'Home Page',
+        image: { srcPath: 'projects/video-streaming/cover.png' },
+        date: '2024-01-30',
+        link: { url: '#', caption: 'video list' },
+      },
+      {
+        title: 'Video upload and processing',
+        image: { srcPath: 'projects/video-streaming/up.png' },
+        date: '2024-01-30',
+        link: { url: 'https://news.ycombinator.com/front?day=2021-04-16', caption: 'Hacker News archive' },
+      },
+      {
+        title: 'Video streaming',
+        image: { srcPath: 'projects/video-streaming/pl.png' },
+        date: '2021-01-30',
+        link: { url: 'https://www.producthunt.com/posts/js-image-carver', caption: 'Product page' },
+      },
+    ],
+  },
+  'Bookish-A-Book-Management-Site': {
+    id: 'Bookish-A-Book-Management-Site',
+    name: '📚 Bookish- A Book Management Site',
+    server: { url: 'https://github.com/AwalHossain/Bookish' },
+    client: {
+      url: 'https://github.com/AwalHossain/Bookish'
     },
-    'pc-builder': {
-        id: 'pc-builder',
-        name: '💻 PC Builder',
-        // server: { url: "hhttps://github.com/AwalHossain/PC-Builder" },
-        client: {
-            url: "https://github.com/AwalHossain/PC-Builder"
-        },
-        demoURL: { url: "https://pc-builder-chi-seven.vercel.app" },
-        cover: { srcPath: 'projects/pcBuilder/cover.png' },
-        startDate: '2023-08-15',
-        endDate: '2023-08-25',
-        summary: [
-            'Discover and customize your dream PC with our intuitive builder, featuring organized component categories',
-        ],
-        tags: [
-            { name: 'NextJs' },
-            { name: 'Redux Toolkit' },
-            { name: 'Next-auth' },
-            { name: 'Redis' },
-        ],
-        gitHubRepo: {
-            owner: 'AwalHossain',
-            repo: 'video_streaming_service_server',
-        },
-        details: [
-            {
-                title: 'Home page',
-                image: { srcPath: 'projects/pcBuilder/home.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'home page' },
-            },
-            {
-                title: 'Product Details',
-                image: { srcPath: 'projects/pcBuilder/detail.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'PC Builder page' },
-            },
-            {
-                title: 'Pc Builder',
-                image: { srcPath: 'projects/pcBuilder/pcbuild.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'PC Builder page' },
-            },
-        ],
+    demoURL: { url: 'https://bookishly.netlify.app/' },
+    cover: { srcPath: 'projects/bookish/cover.png' },
+    startDate: '2023-09-18',
+    endDate: '2023-09-27',
+    summary: [
+      'Bookish is a web application that allows users to discover, review, and manage books',
+    ],
+    tags: [
+      { name: 'ReactJS' },
+      { name: 'NodeJs' },
+      { name: 'ExpressJs' },
+      { name: 'MongoDB' },
+      { name: 'RTK' },
+      { name: 'Tailwind CSS' },
+    ],
+    gitHubRepo: {
+      owner: 'AwalHossain',
+      repo: 'video_streaming_service_server',
     },
-    'health-coach': {
-        id: 'health-coach',
-        name: '🩺 Health-Coach',
-        // server: { url: "hhttps://github.com/AwalHossain/PC-Builder" },
-        client: {
-            url: "https://github.com/AwalHossain/-healthcare-related-website-AwalHossain"
-        },
-        demoURL: { url: "https://healthcare-services-c55cf.web.app" },
-        cover: { srcPath: 'projects/health/cover.png' },
-        startDate: '2022-06-25',
-        endDate: '2022-06-29',
-        summary: [
-            'Healthcoach is your all-in-one platform for achieving holistic wellness, offering personalized coaching, diverse health options, and expert guidance.',
-        ],
-        tags: [
-            { name: 'ReactJs' },
-            { name: 'ReduxJs' },
-            { name: 'Firebase' },
-            { name: 'MongoDB' },
-        ],
-        gitHubRepo: {
-            owner: 'AwalHossain',
-            repo: '-healthcare-related-website-AwalHossain',
-        },
-        details: [
-            {
-                title: 'Home page',
-                image: { srcPath: 'projects/health/home.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'home page' },
-            },
-            {
-                title: 'Login Page',
-                image: { srcPath: 'projects/health/login.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'Loging page' },
-            },
-            {
-                title: 'Specialists',
-                image: { srcPath: 'projects/health/specialist.png' },
-                date: '2023-08-30',
-                link: { url: '#', caption: 'Specialist' },
-            },
-        ],
+    details: [
+      {
+        title: 'Add new Book to the list',
+        image: { srcPath: 'projects/bookish/addBook.png' },
+        date: '2023-08-30',
+        link: { url: '', caption: '' },
+      },
+      {
+        title: 'Home page',
+        image: { srcPath: 'projects/bookish/home.png' },
+        date: '2023-07-30',
+        link: { url: '', caption: '' },
+      },
+      {
+        title: 'Reading List',
+        image: { srcPath: 'projects/bookish/read.png' },
+        date: '2023-07-30',
+        link: { url: '', caption: '' },
+      },
+    ],
+  },
+  'DevBlog-a-developer-blog-site': {
+    id: 'DevBlog-a-developer-blog-site',
+    name: '📝 DevBlog: Developer Community Blog Site',
+    // server: { url: "https://github.com/AwalHossain/Dev-Blog" },
+    client: {
+      url: 'https://github.com/AwalHossain/Dev-Blog'
     },
-}
+    demoURL: { url: 'https://bookishly.netlify.app/' },
+    cover: { srcPath: 'projects/devblog/cover.png' },
+    startDate: '2023-06-25',
+    endDate: '2023-07-12',
+    summary: [
+      'Dev-Blog is a user-friendly blog site featuring several blogs on various topics related to development',
+    ],
+    tags: [
+      { name: 'JavaScript' },
+      { name: 'TypeScript' },
+      { name: 'Redux Toolkit' },
+      { name: 'NodeJs' },
+      { name: 'ExpressJs' },
+      { name: 'Tailwind CSS' },
+    ],
+    gitHubRepo: {
+      owner: 'AwalHossain',
+      repo: 'Dev-Blog',
+    },
+    details: [
+      {
+        title: 'Blog post list',
+        image: { srcPath: 'projects/devblog/cover.png' },
+        date: '2023-04-30',
+        link: { url: '', caption: '' },
+      },
+      {
+        title: 'Blog Details',
+        image: { srcPath: 'projects/devblog/detail.png' },
+        date: '2023-04-30',
+        link: { url: '', caption: '' },
+      },
+      {
+        title: 'Save Blog to Reading List',
+        image: { srcPath: 'projects/devblog/save.png' },
+        date: '2023-04-30',
+        link: { url: '', caption: '' },
+      },
+    ],
+  },
+  'pc-builder': {
+    id: 'pc-builder',
+    name: '💻 PC Builder',
+    // server: { url: "hhttps://github.com/AwalHossain/PC-Builder" },
+    client: {
+      url: 'https://github.com/AwalHossain/PC-Builder'
+    },
+    demoURL: { url: 'https://pc-builder-chi-seven.vercel.app' },
+    cover: { srcPath: 'projects/pcBuilder/cover.png' },
+    startDate: '2023-08-15',
+    endDate: '2023-08-25',
+    summary: [
+      'Discover and customize your dream PC with our intuitive builder, featuring organized component categories',
+    ],
+    tags: [
+      { name: 'NextJs' },
+      { name: 'Redux Toolkit' },
+      { name: 'Next-auth' },
+      { name: 'Redis' },
+    ],
+    gitHubRepo: {
+      owner: 'AwalHossain',
+      repo: 'video_streaming_service_server',
+    },
+    details: [
+      {
+        title: 'Home page',
+        image: { srcPath: 'projects/pcBuilder/home.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'home page' },
+      },
+      {
+        title: 'Product Details',
+        image: { srcPath: 'projects/pcBuilder/detail.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'PC Builder page' },
+      },
+      {
+        title: 'Pc Builder',
+        image: { srcPath: 'projects/pcBuilder/pcbuild.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'PC Builder page' },
+      },
+    ],
+  },
+  'health-coach': {
+    id: 'health-coach',
+    name: '🩺 Health-Coach',
+    // server: { url: "hhttps://github.com/AwalHossain/PC-Builder" },
+    client: {
+      url: 'https://github.com/AwalHossain/-healthcare-related-website-AwalHossain'
+    },
+    demoURL: { url: 'https://healthcare-services-c55cf.web.app' },
+    cover: { srcPath: 'projects/health/cover.png' },
+    startDate: '2022-06-25',
+    endDate: '2022-06-29',
+    summary: [
+      'Healthcoach is your all-in-one platform for achieving holistic wellness, offering personalized coaching, diverse health options, and expert guidance.',
+    ],
+    tags: [
+      { name: 'ReactJs' },
+      { name: 'ReduxJs' },
+      { name: 'Firebase' },
+      { name: 'MongoDB' },
+    ],
+    gitHubRepo: {
+      owner: 'AwalHossain',
+      repo: '-healthcare-related-website-AwalHossain',
+    },
+    details: [
+      {
+        title: 'Home page',
+        image: { srcPath: 'projects/health/home.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'home page' },
+      },
+      {
+        title: 'Login Page',
+        image: { srcPath: 'projects/health/login.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'Loging page' },
+      },
+      {
+        title: 'Specialists',
+        image: { srcPath: 'projects/health/specialist.png' },
+        date: '2023-08-30',
+        link: { url: '#', caption: 'Specialist' },
+      },
+    ],
+  },
+};
