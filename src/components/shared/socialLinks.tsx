5bb4e2b4f95667a8ed3d2adaf8f01e33d079ede4
Awalhossain <waqin21@gmail.com>
2024-02-04 11:27:23 +0600

    Refactor socialLinks component

--- src/components/shared/socialLinks.tsx
+++ src/components/shared/socialLinks.tsx
@@ -22,58 +22,58 @@ type SocialLinksProps = {
 };
 
 const linkToIcon = {
-    [socialLinkTypes.twitter]: FaTwitter,
-    [socialLinkTypes.instagram]: FaInstagram,
-    [socialLinkTypes.gitHub]: FaGithub,
-    [socialLinkTypes.stackOverflow]: FaStackOverflow,
-    [socialLinkTypes.linkedIn]: FaLinkedin,
-    [socialLinkTypes.medium]: FaMedium,
-    [socialLinkTypes.facebook]: FaFacebookSquare,
-    [socialLinkTypes.devTo]: FaDev,
-    [socialLinkTypes.youTube]: FaYoutube,
+  [socialLinkTypes.twitter]: FaTwitter,
+  [socialLinkTypes.instagram]: FaInstagram,
+  [socialLinkTypes.gitHub]: FaGithub,
+  [socialLinkTypes.stackOverflow]: FaStackOverflow,
+  [socialLinkTypes.linkedIn]: FaLinkedin,
+  [socialLinkTypes.medium]: FaMedium,
+  [socialLinkTypes.facebook]: FaFacebookSquare,
+  [socialLinkTypes.devTo]: FaDev,
+  [socialLinkTypes.youTube]: FaYoutube,
 };
 
 const SocialLinks = (props: SocialLinksProps): React.ReactElement | null => {
-    const {
-        links = [],
-        expandable = true,
-        iconClassName = 'w-6 h-6',
-        itemClassName = 'mr-4 mb-2',
-    } = props;
+  const {
+    links = [],
+    expandable = true,
+    iconClassName = 'w-6 h-6',
+    itemClassName = 'mr-4 mb-2',
+  } = props;
 
-    if (!links) {
-        return null;
+  if (!links) {
+    return null;
+  }
+
+  const filteredLinks = links.filter((link) => !link?.hidden);
+
+  const onRender = (socialLink: SocialLinkType): React.ReactElement => {
+    let linkIcon = null;
+    const linkType = socialLink?.type || '';
+    if (linkType in linkToIcon) {
+      const Icon = linkToIcon[linkType];
+      linkIcon = <Icon className={iconClassName} />;
     }
-
-    const filteredLinks = links.filter((link) => !link?.hidden);
-
-    const onRender = (socialLink: SocialLinkType): React.ReactElement => {
-        let linkIcon = null;
-        const linkType = socialLink?.type || '';
-        if (linkType in linkToIcon) {
-            const Icon = linkToIcon[linkType];
-            linkIcon = <Icon className={iconClassName} />;
-        }
-        return (
-            <HyperLink link={socialLink}>
-                {linkIcon || socialLink.url}
-            </HyperLink>
-        );
-    };
-
-    const toHide = (socialLink: SocialLinkType): boolean => {
-        return typeof socialLink.secondary !== 'boolean' || socialLink.secondary;
-    };
-
     return (
-        <Expander
-            items={filteredLinks}
-            onRender={onRender}
-            toHide={toHide}
-            expandable={expandable}
-            itemClassName={itemClassName}
-        />
+      <HyperLink link={socialLink}>
+        {linkIcon || socialLink.url}
+      </HyperLink>
     );
+  };
+
+  const toHide = (socialLink: SocialLinkType): boolean => {
+    return typeof socialLink.secondary !== 'boolean' || socialLink.secondary;
+  };
+
+  return (
+    <Expander
+      items={filteredLinks}
+      onRender={onRender}
+      toHide={toHide}
+      expandable={expandable}
+      itemClassName={itemClassName}
+    />
+  );
 };
 
 export default SocialLinks;
