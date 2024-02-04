b7645e9cbc23e209d552c681763a4aed05bcff4b
Awalhossain <waqin21@gmail.com>
2024-02-04 11:26:49 +0600

    Refactor HyperLink component

--- src/components/shared/HyperLink.tsx
+++ src/components/shared/HyperLink.tsx
@@ -16,66 +16,68 @@ export type HyperLinkProps = {
 };
 
 const HyperLink = (props: HyperLinkProps): React.ReactElement | null => {
-    const {
-        link,
-        children,
-        className = '',
-        activeClassName = '',
-        separatorClassName = 'w-2',
-        hoverClassName = null,
-        startEnhancer = null,
-        formatted = true,
-        /* eslint-disable-next-line @typescript-eslint/no-empty-function */
-        onClick = (): void => { },
-    } = props;
+  const {
+    link,
+    children,
+    className = '',
+    activeClassName = '',
+    separatorClassName = 'w-2',
+    hoverClassName = null,
+    startEnhancer = null,
+    formatted = true,
+    /* eslint-disable-next-line @typescript-eslint/no-empty-function */
+    onClick = (): void => { },
+  } = props;
 
-    if (!link?.url) {
-        return null;
-    }
+  if (!link?.url) {
+    return null;
+  }
 
-    const hoverClasses = hoverClassName || 'hover:text-red-600';
+  const hoverClasses = hoverClassName || 'hover:text-red-600';
 
-    const commonClasses = formatted
-        ? `transition duration-200 ease-in-out flex flex-row items-center ${hoverClasses}`
-        : '';
+  const commonClasses = formatted
+    ? `transition duration-200 ease-in-out flex flex-row items-center ${hoverClasses}`
+    : '';
 
-    const caption = link?.caption || undefined;
+  const caption = link?.caption || undefined;
 
-    const isExternal = link.url.startsWith('http');
-    const isHash = link.url.startsWith('#');
+  const isExternal = link.url.startsWith('http');
+  const isHash = link.url.startsWith('#');
 
-    const separator = startEnhancer ? (
-        <span className={separatorClassName} />
-    ) : null;
+  const separator = startEnhancer ? (
+    <span className={separatorClassName} />
+  ) : null;
 
-    const externalLink = (
-        <a
-            href={link.url}
-            className={`${commonClasses} ${className}`}
-            onClick={onClick}
-            title={caption}
-        >
-            {formatted && startEnhancer}
-            {formatted && separator}
-            {children}
-        </a>
-    );
+  const externalLink = (
+    <a
+      href={link.url}
+      className={`${commonClasses} ${className}`}
+      onClick={onClick}
+      title={caption}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {formatted && startEnhancer}
+      {formatted && separator}
+      {children}
+    </a>
+  );
 
-    const internalLink = (
-        <Link
-            to={link.url}
-            activeClassName={activeClassName}
-            className={`${commonClasses} ${className}`}
-            onClick={onClick}
-            title={caption}
-        >
-            {formatted && startEnhancer}
-            {formatted && separator}
-            {children}
-        </Link>
-    );
+  const internalLink = (
+    <Link
+      to={link.url}
+      activeClassName={activeClassName}
+      className={`${commonClasses} ${className}`}
+      onClick={onClick}
+      title={caption}
+    >
+      {formatted && startEnhancer}
+      {formatted && separator}
+      {children}
+    </Link>
+  );
 
-    return isExternal || isHash ? externalLink : internalLink;
+  return isExternal || isHash ? externalLink : internalLink;
 };
 
 export default HyperLink;
