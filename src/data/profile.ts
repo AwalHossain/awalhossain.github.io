d73b6376eeeb48bef332807831a147dbe9670ba7
Awalhossain <waqin21@gmail.com>
2024-02-04 11:27:13 +0600

    Update profile summary in profile.ts

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,26 +1,26 @@
-//@ts-ignore
+// @ts-ignore
 import { socialLinks } from './socialLinks';
 
 export const profile = {
-    firstName: 'Awal',
-    lastName: 'Hossain',
-    position: 'Currently Seeking a Job',
-    summary: [
-        'Author of 170k ★️ javascript-algorithms repo on GitHub',
-    ],
-    avatar: {
-        srcPath: 'avatar.jpg',
-        caption: 'Awal Hossain',
-    },
-    location: {
-        name: 'Dhaka, The Bangladesh',
-    },
-    tags: [
-        { name: 'Full Stack' },
-        { name: 'JavaScript' },
-        { name: 'Typescript' },
-        { name: 'React' },
-        { name: 'Node' },
-    ],
-    socialLinks,
+  firstName: 'Awal',
+  lastName: 'Hossain',
+  position: 'Currently Seeking a Job',
+  summary: [
+    '',
+  ],
+  avatar: {
+    srcPath: 'avatar.jpg',
+    caption: 'Awal Hossain',
+  },
+  location: {
+    name: 'Dhaka, The Bangladesh',
+  },
+  tags: [
+    { name: 'Full Stack' },
+    { name: 'JavaScript' },
+    { name: 'Typescript' },
+    { name: 'React' },
+    { name: 'Node' },
+  ],
+  socialLinks,
 };
