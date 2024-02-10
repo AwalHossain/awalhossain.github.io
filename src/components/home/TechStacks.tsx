a9bdf5cd24127da665849f3abcb336038e39dec6
Awalhossain <waqin21@gmail.com>
2024-02-10 20:16:42 +0600

    Update user authentication logic

--- src/components/home/TechStacks.tsx
+++ src/components/home/TechStacks.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import aws from '../../images/tech/aws.svg';
+import azure from '../../images/tech/azure.svg';
+import css from '../../images/tech/css.svg';
+import docker from '../../images/tech/docker.svg';
+import gatsby from '../../images/tech/gatsby.svg';
+import html from '../../images/tech/html.svg';
+import js from '../../images/tech/js.svg';
+import mongodb from '../../images/tech/mongodb.svg';
+import next from '../../images/tech/next.svg';
+import node from '../../images/tech/node.svg';
+import postgres from '../../images/tech/postgres.svg';
+import react from '../../images/tech/react.svg';
+import redis from '../../images/tech/redis.svg';
+import redux from '../../images/tech/redux.svg';
+import tailwind from '../../images/tech/tailwind.svg';
+import typescript from '../../images/tech/typescript.svg';
 import './TechStacks.css';
 
 const clients = [
@@ -21,68 +37,75 @@ const ClientCard = ({ name, image }: { name: string, image: string }) => (
 
 const TechStacks = () => (
   <section className="clients my-20 py-4">
-    <div className="max-w-[960px] m-auto">
+    <div className=" m-auto overflow-hidden">
       <div className=" text-center mb-20 relative">
-        <h1 className="font-serif text-black text-5xl font-normal pb-5">Clients</h1>
-        <p className="font-serif text-black text-2xl mb-4">We’ve been working with the teams around the world.</p>
+        <h1 className="font-serif text-black text-5xl font-normal pb-5">Skills</h1>
+        <p className="font-serif text-black text-2xl mb-4">I’ve been working with the skills.</p>
         <div className="absolute w-10 h-0.5 bg-red-500 left-1/2 transform -translate-x-1/2" />
       </div>
       <div className="content-group flex flex-col lg:flex-row justify-center">
         <div className="content-card flex flex-col justify-center">
 
-          <div className="client-item hoopsai flex justify-center items-center  bg-gray-100 w-4/6 h-20 lg:h-28 lg:w-44
-           border border-transparent rounded-lg m-2 "
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/YCxRWyMj/js.png" alt="name" />
+          <div className="client-item one hoopsai flex justify-center items-center bg-gray-100 w-full lg:w-3/6 h-20 lg:h-28 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={html} alt="name" />
           </div>
 
         </div>
         <div className="content-card flex flex-col justify-center">
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100  w-4/5 h-20 lg:h-28 lg:w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/k51yj6Vm/react.png" alt="name" />
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 w-full md:w-3/12 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={css} alt="name" />
           </div>
-          <div className="client-item hoopsai  hidden lg:flex justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/W1VStPRQ/node.png" alt="name" />
+          <div className="client-item hoopsai two flex justify-center items-center bg-gray-100 w-full md:w-3/12 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={js} alt="name" />
           </div>
         </div>
         <div className="content-card flex flex-col justify-center">
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100 h-28 w-44
-            border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/6QDnyDTk/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png" alt="name" />
+          <div className="client-item hoopsai three flex justify-center items-center bg-gray-100 w-full lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={typescript} alt="name" />
           </div>
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/Cx1CRCLV/db.png" alt="name" />
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 w-full lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={node} alt="name" />
           </div>
-          <div className="client-item hoopsai  justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.ibb.co/frTT8Y2/My-SQL-Logo.png" alt="name" />
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 w-full lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={tailwind} alt="name" />
           </div>
         </div>
         <div className="content-card flex flex-col justify-center">
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/L8vFtGrT/aws-logo-smile-1200x630.png" alt="name" />
+          <div className="client-item react four hoopsai flex justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={react} alt="name" />
           </div>
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/xT0mxzXP/azure-1.webp" alt="name" />
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={next} alt="name" />
+          </div>
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={gatsby} alt="name" />
+          </div>
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={redux} alt="name" />
           </div>
         </div>
         <div className="content-card flex flex-col justify-center">
-          <div className="client-item hoopsai flex justify-center items-center bg-gray-100  h-28 w-44
-           border border-transparent rounded-lg m-2"
-          >
-            <img className="w-28 align-middle" src="https://i.postimg.cc/25St8Lgq/download.png" alt="name" />
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={mongodb} alt="name" />
+          </div>
+          <div className="client-item five hoopsai flex justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={postgres} alt="name" />
+          </div>
+          <div className="client-item hoopsai lg:flex hidden justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={redis} alt="name" />
+          </div>
+        </div>
+        <div className="content-card flex flex-col justify-center">
+          <div className="client-item hoopsai flex six justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={docker} alt="name" />
+          </div>
+          <div className="client-item lg:flex hidden hoopsai  justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={azure} alt="name" />
+          </div>
+        </div>
+        <div className="content-card  flex flex-col justify-center">
+          <div className="client-item seven hoopsai flex justify-center items-center bg-gray-100 lg:w-3/5 h-20 lg:h-24 rounded-lg m-2">
+            <img className="object-contain w-full h-full" src={aws} alt="name" />
           </div>
         </div>
       </div>
