8c48aa0ab78a01db35d139bf2499afbd1b395836
Awalhossain <waqin21@gmail.com>
2024-02-10 20:16:18 +0600

    Refactor code to improve performance and readability

--- src/components/home/LatestProject.tsx
+++ src/components/home/LatestProject.tsx
@@ -11,72 +11,69 @@ const LatestProjects = () => {
     const mernID = ("Reely-A-MERN-Video-Streaming-App");
 
     return (
-        <div className='container max-w-6xl pb-5 my-10 font-custom'>
-            <div className='w-full max-w-5xl m-auto'>
-                <div className='mt-24 mx-0 -mb-24 space-y-20'>
-                    <div className='relative text-center'>
-                        <h1 className='text-4xl font-custom font-[600] text-center leading-4 tracking-wide'>Latest Works</h1>
-                        <span className="absolute w-10 h-0.5 bg-red-500 bottom-[-1.5rem] left-1/2 transform -translate-x-1/2"></span>
-                    </div>
-                    <div className="grid grid-cols-1  md:grid-cols-[60%,40%] gap-4 bg-gray-100 shadow-xl rounded-lg">
-                        <div className='text-center rounded-t-lg md:rounded-l-l overflow-hidden'>
-                            <img className='w-full md:min-h-[452px] h-auto object-cover' src={mern} alt="Album" />
-                        </div>
-                        <div className="p-4 flex flex-col justify-center items-center">
-                            <h2 className="text-4xl pb-5 font-bold ">New album is released!</h2>
-                            <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
-                            <Link
-                                to={`${routes.projects.path}/${mernID}#details`}
-                                title={"caption"}
-                                className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
-                            >
-                                <span>view project</span>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-1  md:grid-cols-[60%,40%] gap-4 bg-gray-100 shadow-xl rounded-lg">
-                        <div className='text-center rounded-t-lg md:rounded-l-l overflow-hidden'>
-                            <img className='w-full md:min-h-[452px] h-auto object-cover' src={book} alt="Album" />
-                        </div>
-                        <div className="p-4 flex flex-col justify-center items-center">
-                            <h2 className="text-4xl pb-5 font-bold">New album is released!</h2>
-                            <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
-                            <Link
-                                to={`${routes.projects.path}/${mernID}#details`}
-                                title={"caption"}
-                                className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
-                            >
-                                <span>view project</span>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-1  md:grid-cols-[60%,40%] gap-4 bg-white-500 shadow-xl rounded-lg">
-                        <div className='text-center rounded-t-lg md:rounded-l-lg overflow-hidden'>
-                            <img className='w-full md:min-h-[452px] h-auto object-cover' src={blog} alt="Album" />
-                        </div>
-                        <div className="p-4 flex flex-col justify-center items-center">
-                            <h2 className="text-4xl pb-5 font-bold">DEVeloper Blog!</h2>
-                            <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
-                            <div >
-                                <Link
-                                    to={`${routes.projects.path}/${mernID}#details`}
-                                    title={"caption"}
-                                    className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
-                                >
-                                    <span>view project</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
+
+        <div className='mt-24 mx-0 -mb-24 space-y-20'>
+            <div className='relative text-center'>
+                <h1 className='text-4xl font-custom font-[600] text-center leading-4 tracking-wide'>Latest Works</h1>
+                <span className="absolute w-10 h-0.5 bg-red-500 bottom-[-1.5rem] left-1/2 transform -translate-x-1/2"></span>
+            </div>
+            <div className="grid grid-cols-1  md:grid-cols-2 gap-4 latest shadow-xl rounded-lg">
+                <div className='text-center rounded-t-lg md:rounded-l-l overflow-hidden'>
+                    <img className='w-full md:min-h-[452px] h-auto object-cover' src={mern} alt="Album" />
+                </div>
+                <div className="p-4 flex flex-col justify-center items-center">
+                    <h2 className="text-4xl pb-5 font-bold ">New album is released!</h2>
+                    <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
                     <Link
                         to={`${routes.projects.path}/${mernID}#details`}
                         title={"caption"}
-                        className="block font-custom2 w-[220px] text-red-600 h-[50px] py-4 px-8 text-sm lg:text-[14px] font-semibold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] outer"
+                        className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
                     >
-                        <span>see more</span>
+                        <span>view project</span>
                     </Link>
                 </div>
             </div>
+            <div className="grid grid-cols-1 latest  md:grid-cols-2 gap-4  shadow-xl rounded-lg">
+                <div className='text-center rounded-t-lg md:rounded-l-l overflow-hidden'>
+                    <img className='w-full md:min-h-[452px] h-auto object-cover' src={book} alt="Album" />
+                </div>
+                <div className="p-4 flex flex-col justify-center items-center">
+                    <h2 className="text-4xl pb-5 font-bold">New album is released!</h2>
+                    <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
+                    <Link
+                        to={`${routes.projects.path}/${mernID}#details`}
+                        title={"caption"}
+                        className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
+                    >
+                        <span>view project</span>
+                    </Link>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 latest  md:grid-cols-2 gap-4 shadow-xl rounded-lg">
+                <div className='text-center rounded-t-lg md:rounded-l-lg overflow-hidden'>
+                    <img className='w-full md:min-h-[452px] h-auto object-cover' src={blog} alt="Album" />
+                </div>
+                <div className="p-4 flex flex-col justify-center items-center">
+                    <h2 className="text-4xl pb-5 font-bold">DEVeloper Blog!</h2>
+                    <p className='text-sm lg:text-[18px] font-medium text-gray-400'>Click the button to listen on Spotiwhy app.</p>
+                    <div >
+                        <Link
+                            to={`${routes.projects.path}/${mernID}#details`}
+                            title={"caption"}
+                            className="block font-custom2 text-white tracking-wider w-[220px] h-[50px] py-4 px-8 text-sm lg:text-[12px] font-bold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] view-project-first"
+                        >
+                            <span>view project</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <Link
+                to={`${routes.projects.path}/${mernID}#details`}
+                title={"caption"}
+                className="block font-custom2 w-[220px] text-red-600 h-[50px] py-4 px-8 text-sm lg:text-[14px] font-semibold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] outer"
+            >
+                <span>see more</span>
+            </Link>
         </div>
     )
 }
