3f9e1f0488f6d6d6939620873b4e92ee32355471
awalhossain <waqin21@gmail.com>
2024-02-12 01:05:36 +0600

    Add ContactButton component

---
+++ src/components/home/ContactButton.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'gatsby'
+import React, { useState, useEffect, useRef } from 'react'
+import { routes } from '../constants/routes'
+import './Button.css'
+
+const ContactButton = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        const checkScroll = () => {
+            setIsScrolled(window.scrollY >= 20);
+        };
+
+        window.addEventListener('scroll', checkScroll);
+
+        return () => {
+            window.removeEventListener('scroll', checkScroll);
+        };
+    }, []);
+
+    return (
+        <div className={`py-5 hero-button-float ${isScrolled ? 'scrolled' : ''}`}>
+            <div className='button-wrapper'>
+                <Link
+                    ref={buttonRef}
+                    to={`${routes.projects.path}/#details`}
+                    title={"caption"}
+                    className="btn block font-custom2 w-[180px] bg-red-600 text-white h-[50px] py-4 px-8 text-sm lg:text-[14px] font-semibold text-center leading-5 uppercase rounded-3xl border-0 mt-8 lg:mt-[32px] outer"
+                >
+                    <span>Let's Talk</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default ContactButton
