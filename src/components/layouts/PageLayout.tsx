55b295e306ac6ceee381a7258e37ac045c951a9e
awalhossain <waqin21@gmail.com>
2024-02-17 02:05:14 +0600

    Update PageLayout component to include dark mode background color

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -14,7 +14,7 @@ const PageLayout = (props: PageLayoutProps): React.ReactElement | null => {
   }
 
   return (
-    <main className="flex flex-col items-center w-full">
+    <main className="flex flex-col items-center w-full dark:bg-gray-950">
       <div className="max-w-screen-xl self-stretch m-auto w-full overflow-x-hidden">
         <Header className="px-6 sm:px-12 py-6" />
         <article className="px-6 sm:px-12 pb-16 mb-8">
