8bb5749cdd34ccdbfa0c5e9af8c171ce1f9e24ff
awalhossain <waqin21@gmail.com>
2024-02-17 02:06:06 +0600

    Update Tag component styling

--- src/components/shared/Tag.tsx
+++ src/components/shared/Tag.tsx
@@ -12,7 +12,7 @@ const Tag = (props: TagProps): React.ReactElement => {
   return (
     <span
       key={tag.name}
-      className="inline-block bg-gray-200 rounded-sm px-2 py-1 text-xs font-normal last:mr-0"
+      className="inline-block bg-gray-200 dark:bg-gray-800 rounded-sm px-2 py-1 text-xs font-normal last:mr-0"
     >
       {tag.name}
     </span>
