c887d226bf0d270b81da35ca9d8fcd09d4c63430
Awalhossain <waqin21@gmail.com>
2024-05-29 17:11:26 +0600

    refactor: Update Projects component to use PageProps and remove console.log statement

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,20 @@
+import { PageProps } from 'gatsby';
 import React from 'react';
-
 import ProjectScreens from '../components/screens/ProjectScreens';
 import { projects } from '../data/projects';
+import { BlogPageQuery } from './__generated__/BlogPageQuery';
 
-const Projects = (): React.ReactElement => {
-  return (
-    <ProjectScreens projects={projects} />
-  );
+interface BlogProps extends PageProps {
+  data: BlogPageQuery;
+}
+
+const Projects = (props: any): React.ReactElement => {
+  const { data } = props;
+  console.log(data, "data checking from projects");
+
+
+
+  return <ProjectScreens projects={projects} />;
 };
 
 export default Projects;
